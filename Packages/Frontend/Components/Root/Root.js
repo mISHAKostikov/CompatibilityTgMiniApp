fe1855a7140a359d30839ebc27d8afa02f3f78de
MihaDota2 <sergeysergey46934@gmail.com>
2024-06-25 00:30:23 +0300

    0.0.6
    second page

--- Packages/Frontend/Components/Root/Root.js
+++ Packages/Frontend/Components/Root/Root.js
@@ -1,8 +1,11 @@
 import {Component} from '../../Api/Components/Component/Component.js';
 import {Form} from '../Form/Form.js';
+<<<<<<< Updated upstream
 import {Leafable} from '../../Api/Components/Leafable/Leafable.js';
 import {ResultPage} from '../ResultPage/ResultPage.js';
 
+=======
+>>>>>>> Stashed changes
 
 
 export class Root extends Component {
