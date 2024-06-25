28c84c48000d9180b45e338e081af22aa177b7cb
MSC <95544952+mISHAKostikov@users.noreply.github.com>
2024-06-26 00:16:54 +0300

    0.0.9
    Button

--- Packages/Frontend/Components/Button/Button.js
+++ Packages/Frontend/Components/Button/Button.js
@@ -6,7 +6,36 @@ export class Button extends Component {
     static html_url = true;
     static url = import.meta.url;
 
+    static _attributes = {
+        ...super._attributes,
+
+        _animation: false,
+    }
+
     static {
         this.define();
+    }
+
+    get _animation() {
+        return this._attributes._animation;
+    }
+    set _animation(animation) {
+        this._attribute__set('_animation', animation);
+    }
+
+
+    _eventListeners__define() {
+        this.eventListeners__add({
+            pointerdown: this._on_pointerDown,
+            transitionend: this._on_transitionEnd,
+        });
+    }
+
+    _on_pointerDown() {
+        this._animation = true;
+    }
+
+    _on_transitionEnd() {
+        this._animation = false;
     }
 }
