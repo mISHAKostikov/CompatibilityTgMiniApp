4ba2bcdfee1fe604e85c47dbd160043c0fb18cd3
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-07-06 17:08:55 +0300

    0.0.12
    Flickable add

--- Packages/Frontend/Components/ResultPage/ResultPage.js
+++ Packages/Frontend/Components/ResultPage/ResultPage.js
@@ -1,13 +1,86 @@
 import {Component} from '../../Api/Components/Component/Component.js';
+import {Flickable} from '../../Api/Components/Flickable/Flickable.js';
+import {Repeater} from '../../Api/Components/Repeater/Repeater.js';
+
 import {Pie} from '../Pie/Pie.js';
 
 
 export class ResultPage extends Component {
+    static _components = [Flickable, Repeater, Pie];
+
+    static _elements = {
+        display: '',
+        repeater: '',
+    };
+
     static css_url = true;
     static html_url = true;
     static url = import.meta.url;
 
+    static Repeater_manager = class extends Repeater.Manager {
+        data__apply() {
+            this._item.progress = this._model_item.progress;
+        }
+
+        init() {
+            this.data__apply();
+        }
+    };
+
     static {
         this.define();
+    }
+
+
+    _eventListeners__define() {
+        this._elements.repeater.eventListeners__add({
+            add: this._repeater__on_add.bind(this),
+            define: this._repeater__on_add.bind(this),
+        });
+        window.addEventListener('resize', this._window__on_resize.bind(this));
+    }
+
+    _init() {
+        this._elements.repeater.Manager = this.constructor.Repeater_manager;
+        this.refresh();
+        this._result__load()
+    }
+
+    _repeater__on_add(event) {
+        this.refresh();
+    }
+
+    _window__on_resize() {
+        this.refresh();
+    }
+
+    _result__load() {
+        let result = [
+            {
+                progress: 99,
+            },
+            {
+                progress: 80,
+            },
+            {
+                progress: 12,
+            },
+            {
+                progress: 99,
+            },
+            {
+                progress: 80,
+            },
+            {
+                progress: 12,
+            },
+        ];
+
+        this._elements.repeater.model.add(result);
+    }
+
+
+    refresh() {
+        this._elements.display.refresh();
     }
 }
