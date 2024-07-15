27b3e25396f571065f7fe2702f5f21d9e6dfc9bf
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-07-15 18:49:27 +0300

    test

--- Packages/Frontend/Components/ResultPage/ResultPage.js
+++ Packages/Frontend/Components/ResultPage/ResultPage.js
@@ -6,7 +6,7 @@ import {Result} from '../Result/Result.js';
 
 
 export class ResultPage extends Component {
-    static _components = [Repeater];
+    static _components = [Flickable, Repeater, Result];
 
     static _elements = {
         button__back: '',
