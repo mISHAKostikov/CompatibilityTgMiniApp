3fff1375f3af091488eb6bfe931b798eaf9df497
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-07-15 19:27:17 +0300

    test

--- Packages/Frontend/Main/Main.js
+++ Packages/Frontend/Main/Main.js
@@ -1,15 +1,19 @@
 import {Root} from '../Components/Root/Root.js';
 
+let falg = false
 
 window.addEventListener('load',() => {
     // window.Telegram.WebApp.expand();
+    a()
 })
 
-    location = location.origin + location.pathname
+
 // let root;
-// function a() {
-//     setTimeout(b, 1e4)
-// }
+function a() {
+    if (flag) return;
+    location = location.origin + location.pathname;
+    flag = true;
+}
 
 // async function b() {
 //     root = await import('../Components/Root/Root.js');
