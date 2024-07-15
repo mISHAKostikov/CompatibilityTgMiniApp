1ff3711670c65da7f03dc1905839133d55ac3e66
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-07-15 13:40:34 +0300

    test

--- Packages/Frontend/Components/Root/Root.js
+++ Packages/Frontend/Components/Root/Root.js
@@ -1,99 +1,102 @@
-import {Component} from '../../Api/Components/Component/Component.js';
-import {Leafable} from '../../Api/Components/Leafable/Leafable.js';
-import {Rest} from '../../Api/Units/Rest/Rest.js';
+// import {Component} from '../../Api/Components/Component/Component.js';
+// import {Leafable} from '../../Api/Components/Leafable/Leafable.js';
+// import {Rest} from '../../Api/Units/Rest/Rest.js';
 
-import {ButtonStart} from '../ButtonStart/ButtonStart.js';
-import {FormPage} from '../FormPage/FormPage.js';
-import {ResultPage} from '../ResultPage/ResultPage.js';
-
-
-export class Root extends Component {
-    static _attributes = {
-        ...super._attributes,
-
-        verticalSwipes: true,
-    }
-
-    static _components = [ResultPage];
-
-    static _elements = {
-        buttonStart: '',
-        formPage: '',
-        leafable: '',
-        resultPage: '',
-    };
+// import {ButtonStart} from '../ButtonStart/ButtonStart.js';
+// import {FormPage} from '../FormPage/FormPage.js';
+// import {ResultPage} from '../ResultPage/ResultPage.js';
 
 
-    static css_url = true;
-    static html_url = true;
-    static url = import.meta.url;
+// export class Root extends Component {
+//     static _attributes = {
+//         ...super._attributes,
+
+//         verticalSwipes: true,
+//     }
+
+//     static _components = [ResultPage];
+
+//     static _elements = {
+//         buttonStart: '',
+//         formPage: '',
+//         leafable: '',
+//         resultPage: '',
+//     };
 
 
-    static {
-        this.define();
-    }
+//     static css_url = true;
+//     static html_url = true;
+//     static url = import.meta.url;
 
 
-    _form_data = [];
-    _rest = new Rest(`https://mmnds.store`);
-    _telegram = null;
+//     static {
+//         this.define();
+//     }
 
 
-    get verticalSwipes() {
-        return this._attributes.verticalSwipes;
-    }
-    set verticalSwipes(value) {
-        if (value) {
-            this._telegram.enableVerticalSwipes();
-        }
-        else {
-            this._telegram.disableVerticalSwipes();
-        }
-
-        this._attribute__set('verticalSwipes', !!value);
-    }
+//     _form_data = [];
+//     _rest = new Rest(`https://mmnds.store`);
+//     _telegram = null;
 
 
-    _eventListeners__define() {
-        this._elements.buttonStart.addEventListener('pointerdown', this._buttonStart__on_pointerDown.bind(this));
-        this._elements.formPage.addEventListener('calculate', this._formPage__on_calculate.bind(this));
-        this._elements.resultPage._elements.button__back.addEventListener('pointerdown', this._button__back__on_pointerDown.bind(this));
-    }
+//     get verticalSwipes() {
+//         return this._attributes.verticalSwipes;
+//     }
+//     set verticalSwipes(value) {
+//         if (value) {
+//             this._telegram.enableVerticalSwipes();
+//         }
+//         else {
+//             this._telegram.disableVerticalSwipes();
+//         }
 
-    _button__back__on_pointerDown() {
-       this._elements.leafable.index--;
-       this.verticalSwipes = true;
-    }
+//         this._attribute__set('verticalSwipes', !!value);
+//     }
 
-    async _buttonStart__on_pointerDown() {
-        await this._elements.buttonStart._promise;
-        this._elements.leafable.index++;
-    }
 
-    _formPage__on_calculate(event) {
-        this._form_data = event.detail;
-        this._result__define();
-    }
+//     _eventListeners__define() {
+//         this._elements.buttonStart.addEventListener('pointerdown', this._buttonStart__on_pointerDown.bind(this));
+//         this._elements.formPage.addEventListener('calculate', this._formPage__on_calculate.bind(this));
+//         this._elements.resultPage._elements.button__back.addEventListener('pointerdown', this._button__back__on_pointerDown.bind(this));
+//     }
 
-    async _result__define() {
-        let result = await this._result__receive();
+//     _button__back__on_pointerDown() {
+//        this._elements.leafable.index--;
+//        this.verticalSwipes = true;
+//     }
 
-        if (!result.length) return;
+//     async _buttonStart__on_pointerDown() {
+//         await this._elements.buttonStart._promise;
+//         this._elements.leafable.index++;
+//     }
 
-        this._elements.leafable.index++;
-        this._elements.resultPage.result__insert(result);
-        this.verticalSwipes = false;
-    }
+//     _formPage__on_calculate(event) {
+//         this._form_data = event.detail;
+//         this._result__define();
+//     }
 
-    async _result__receive() {
-        let result = await this._rest.call('compatibility__calc', ...this._form_data);
+//     async _result__define() {
+//         let result = await this._result__receive();
 
-        return result;
-    }
+//         if (!result.length) return;
 
-    _init() {
-        this._telegram = window.Telegram.WebApp;
-        alert(this._telegram.version);
-        this.props__sync('verticalSwipes');
-    }
-}
+//         this._elements.leafable.index++;
+//         this._elements.resultPage.result__insert(result);
+//         this.verticalSwipes = false;
+//     }
+
+//     async _result__receive() {
+//         let result = await this._rest.call('compatibility__calc', ...this._form_data);
+
+//         return result;
+//     }
+
+//     _init() {
+//         this._telegram = window.Telegram.WebApp;
+//         alert(this._telegram.version);
+//         this.props__sync('verticalSwipes');
+//     }
+// }
+
+
+alert(22)
--- Packages/Frontend/Main/Main.js
+++ Packages/Frontend/Main/Main.js
@@ -1,9 +1,6 @@
-// import {Root} from '../Components/Root/Root.js';
+import '../Components/Root/Root.js';
 
 
 window.addEventListener('load',() => {
     window.Telegram.WebApp.expand();
-    alert(2)
 })
-
-alert(3)
