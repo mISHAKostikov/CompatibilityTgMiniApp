fe44d43cd3a4e1d2bf444f268bfadcb4e579097e
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-07-15 19:39:25 +0300

    test

--- Packages/Frontend/Main/Main.js
+++ Packages/Frontend/Main/Main.js
@@ -4,15 +4,15 @@ let flag = false
 
 window.addEventListener('load',() => {
     // window.Telegram.WebApp.expand();
-    a()
+    // a()
 })
 
 
-// let root;
-function a() {
-    if (location == 'https://mishakostikov.github.io/CompatibilityTgMiniApp/') return;
-    location = location.origin + location.pathname;
-}
+// // let root;
+// function a() {
+//     if (location == 'https://mishakostikov.github.io/CompatibilityTgMiniApp/') return;
+//     location = location.origin + location.pathname;
+// }
 
 // async function b() {
 //     root = await import('../Components/Root/Root.js');
